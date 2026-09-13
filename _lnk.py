002be60c7056bf6236ef8d453f390bf1799d0af6
DaShengGuo <159510656+DaShengGuo@users.noreply.github.com>
2026-09-13 17:00:44 +0800

    开源清理: 移除根目录临时脚本与logs跟踪

--- _lnk.py
+++
@@ -1,7 +0,0 @@
-import re
-data = open(r'C:\Users\Public\Desktop\直播伴侣.lnk', 'rb').read()
-tokens = re.split(rb'[^ -~]+', data)
-for t in tokens:
-    s = t.decode('ascii', errors='ignore')
-    if '.exe' in s.lower() or ('\\' in s and len(s) > 6):
-        print(s)
